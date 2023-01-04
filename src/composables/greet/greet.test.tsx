d2d74788a9bf2223842cd73374d2367f47402cc3
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-01-04 10:46:09 +0100

    added the describe block to the greet test file

--- src/composables/greet/greet.test.tsx
+++ src/composables/greet/greet.test.tsx
@@ -1,14 +1,18 @@
 import { Greet } from "./greet";
 import { render, screen } from "@testing-library/react";
 
-test("Greet render on the DOM", () => {
-  render(<Greet />);
-  const result = screen.getByText("Hello");
-  expect(result).toBeInTheDocument();
-});
+describe("Greet Component", () => {
 
-test("Greet render with name", () => {
-  render(<Greet name="Ibrahim" />);
-  const textElement = screen.getByText("Hello Ibrahim");
-  expect(textElement).toBeInTheDocument();
-});
+    test("renders on the DOM", () => {
+        render(<Greet />);
+        const result = screen.getByText("Hello");
+        expect(result).toBeInTheDocument();
+    });
+    
+    test("renders with a name", () => {
+        render(<Greet name="Ibrahim" />);
+        const textElement = screen.getByText("Hello Ibrahim");
+        expect(textElement).toBeInTheDocument();
+    });
+    
+})
