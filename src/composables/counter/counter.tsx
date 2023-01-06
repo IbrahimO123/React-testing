ff95998412b681833a044b6cb2b7c4d15a97c4e3
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-01-06 13:17:53 +0100

    Testing using queryBy and findBy

--- src/composables/counter/counter.tsx
+++ src/composables/counter/counter.tsx
@@ -4,7 +4,7 @@ export const Counter = () => {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>Counter {count}</h1>
+      <h1>{count}</h1>
       <button onClick={() => setCount((prev) => prev + 1)}>increment</button>
     </div>
   );
