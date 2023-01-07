377dedfb07fad3979d887adb16f6deb88c6061a7
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-01-07 14:13:04 +0100

    testing http request using msw

--- src/composables/user/user.test.tsx
+++ src/composables/user/user.test.tsx
@@ -1,10 +1,32 @@
-import {render, screen} from '@testing-library/react';
-import { User } from './user';
+import { render, screen } from "@testing-library/react";
+import { User } from "./user";
+import { server } from "../../mocks/server";
+import { rest } from "msw";
 
 describe("Users", () => {
-    test("render correctly" , () => {
-        render(<User/>)
-        const headingElement = screen.getByRole("heading")
-        expect(headingElement).toBeInTheDocument()
-    })
-})
+  test("render correctly", () => {
+    render(<User />);
+    const headingElement = screen.getByRole("heading");
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test("renders the number of users", async () => {
+    render(<User />);
+    const listElement = await screen.findAllByRole("listitem");
+    expect(listElement).toHaveLength(3);
+  });
+
+  test("render error messsage when request failed", async () => {
+    server.use(
+      rest.get(
+        "https://jsonplaceholder.typicode.com/users",
+        (req, res, ctx) => {
+          return res(ctx.status(500));
+        }
+      )
+    );
+    render(<User />);
+    const errorElement = await screen.findByText("Error fetching users");
+    expect(errorElement).toBeInTheDocument();
+  });
+});
