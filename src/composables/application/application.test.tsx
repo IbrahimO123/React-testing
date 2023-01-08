13214463be5fb8eb0f67ccd388c63d313062c051
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2023-01-08 18:14:26 +0100

    Added lint staged npm for testing

--- src/composables/application/application.test.tsx
+++ src/composables/application/application.test.tsx
@@ -40,6 +40,6 @@ describe("Application", () => {
 
     const submitElement = screen.getByRole("button");
     expect(submitElement).toBeInTheDocument();
-    expect(submitElement).toBeDisabled();
+    expect(submitElement).not.toBeEnabled();
   });
 });
